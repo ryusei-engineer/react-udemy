60dc8a2aa7207dee1b4f35eca7372da251530799
ryusei-engineer <ryusei.engineer@gmail.com>
2024-07-10 23:51:06 +0900

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import React from 'react';
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <h1>sss</h1>
     <p>a</p>
-    </React.Fragment>
+    </>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1>ヘッダー</h1>
+    </>
+  );
+}
+
+export default App;
